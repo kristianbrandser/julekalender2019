f1f34a04ed3ef4d542a91d3cd6fc66d7374955ba
Kristian Brandser <kristianbrandser@Kristians-MacBook-Pro.local>
2019-12-09 16:04:58 +0100

    Added unittest

--- luke9/luke9.py
+++ luke9/luke9.py
@@ -9,29 +9,38 @@ import unittest
 # splitten kan altså være ujevn). 
 # I tillegg kan ingen av tallene kan være 0.
 
-testkrampusTallrekke = numpy.array([45, 100, 53219, 74742, 11502, 27846, 6834, 38442, 60767])
+testkrampusTallrekke = numpy.array([45, 100, 53219, 74742, 11502, 27846, 6834, 38442, 60767, 45])
 krampusTallrekke = numpy.loadtxt("luke9/krampus.txt", dtype=int)
-#print(len(krampusTallrekke))
-#print(krampusTallrekke)
+sumKrampus = 0
 
 def isKrampus(n):
     n2 = n*n
-    ndigits = int((math.log10(n2)+1))
+    #ndigits = int(math.log10(n2)+1)
+    ndigits = len(str(n2))  
        
     for nn in range(1,ndigits+1):
         a = int(n2/(10**nn))
         b = int(n2%(10**nn))
         if (a+b == n and a != 0 and b != 0):
-            print("Krampus!")
-            print(n, n2, a,b, a+b)
+            print("Fant krampustall: " + str(n))
             return True
+    else: 
+        return False
+    
+for n in krampusTallrekke:
+    if (isKrampus(n)):
+        sumKrampus += n
 
-    return False
+print("Sum av krampustall: " + str(sumKrampus))
 
-print(testkrampusTallrekke)
-for n in testkrampusTallrekke:
-    isKrampus(n)
+class krampusTest(unittest.TestCase):
+    def testKrampetall(self):
+        self.assertEqual(isKrampus(45), True)
+    def testEdgeKrampe(self):
+        self.assertEqual(isKrampus(100), False)
+        self.assertEqual(isKrampus(0), False)
+    def testIkkeKrampetall(self):
+        self.assertEqual(isKrampus(53219), False)
 
-class krampusTest(unittest.TestCase)
-    def test(self):
-        self.assertEqual(isKrampus(45), true)
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_luke9.py
@@ -0,0 +1,5 @@
+import unittest
+
+class krampusTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(isKrampus(45), true)
