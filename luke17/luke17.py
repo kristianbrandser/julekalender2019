51547e435c9d6c36f00d90e418d139cb7f26448d
Kristian Brandser <kristianbrandser@Kristians-MacBook-Pro.local>
2019-12-17 11:16:03 +0100

    Denne mener jeg er løst rett, men får ikke poeng..

---
+++ luke17/luke17.py
@@ -0,0 +1,28 @@
+import math
+import numpy
+
+def rotr(string, n):
+  return string[n:] + string[:n]
+
+def is_square(n):
+  return n > -1 and math.sqrt(n) % 1 == 0
+
+number = 0
+nKvadrattall = numpy.array([], dtype=int)
+
+for i in range(0,999999):
+  #number = number + i
+  #print(number)
+  triangularNumber = int(i*(i+1)/2)
+
+  for j in range (0,len(str(triangularNumber))):
+        rotatedTriangle = int(rotr(str(triangularNumber), j))
+        #if (int(math.sqrt(rotatedTriangle)+0.5)**2 == rotatedTriangle):
+        if (is_square(rotatedTriangle)):
+            print("Triangularnumber: ", triangularNumber)
+            print("Perfekt square found: ", rotatedTriangle)
+            #nKvadrattall += 1
+            nKvadrattall = numpy.append(nKvadrattall, rotatedTriangle)
+  
+print("Antall unike kvadrattall: ", numpy.size(numpy.unique(nKvadrattall)))
+print("Antall kvadrattall: ", numpy.size(nKvadrattall))
