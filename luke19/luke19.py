1cb588ddc43ab19b192e726c69b97826287922e3
Kristian Brandser <kristianbrandser@Kristians-MacBook-Pro.local>
2019-12-20 09:37:54 +0100

    Sum of hidden palindromes is:  659277075458904 Run time:  446 sek...

--- luke19/luke19.py
+++ luke19/luke19.py
@@ -1,14 +1,22 @@
+import time
+
+starttime = time.time()
+
 def isHiddenPalindrome(number):
-    nSum = int(str(number)) + int(str(number)[::-1])
-    return str(nSum) == str(nSum)[::-1]
-    
+    if (str(number) != str(number)[::-1]):
+        nSum = int(str(number)) + int(str(number)[::-1])
+        return str(nSum) == str(nSum)[::-1]
+    return False
+
 assert (isHiddenPalindrome(38) == True)
 assert (isHiddenPalindrome(49) == False)
 
 sumPalindrom = 0
+
 for i in range(1,123454321):
     if (isHiddenPalindrome(i)):
         sumPalindrom += i
-        print("Found hidden palindrom: ", i)
+        #print("Found hidden palindrom: ", i)
         
-print("Sum of hidden palindromes is: ", sumPalindrom) 
+print("Sum of hidden palindromes is: ", sumPalindrom)
+print("Run time: ", time.time() - starttime)
